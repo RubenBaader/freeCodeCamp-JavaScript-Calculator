71dfc3458e4bd5d97c53b6d471b8e828fe142481
RubenBaader <rubenbaader@gmail.com>
2023-03-08 21:25:39 +0100

    added state, event handling scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,67 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+// Behaviour: Calculator
+// Store button input to allow multiple operations before calculating
+// Combination of button presses will alter values (e.g. "3" + "." + "5" => 3.5)
+// Input sanitation: only one decimal (".") per value
+//                   no leading (multiple) zeroes
+// Operand logic   : last pressed operand overwrites
+//     Special case: "-" modifies to negative
+// Pressing "="    : If no new input, redo last operation
+
+// input     :  str = "1+2+5-3*4"
+//   eval(str) => num result 
+//             => setDisplay(result)
+
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [display, setdisplay] = useState<string>("0")
+
+  const handleInput = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const button = e.target as HTMLButtonElement
+    const input = button.innerText
+    // console.log(button.innerText)
+
+    if (display == "0" && input != ".")
+      setdisplay(input);
+    else
+    setdisplay(display + input)
+  }
+
+  const handleClear = () => {
+    // clear state
+    setdisplay("0")
+  }
+
+  const handleCalc = () => {
+    // eval(state);
+  }
 
   return (
     <div className="calculator">
-      <div id='display'> {count} </div>
+      <div id='display' > {display} </div>
       <div className='numbers'>
-        <button id='zero'    > 0 </button>
-        <button id='one'     > 1 </button>
-        <button id='two'     > 2 </button>
-        <button id='three'   > 3 </button>
-        <button id='four'    > 4 </button>
-        <button id='five'    > 5 </button>
-        <button id='six'     > 6 </button>
-        <button id='seven'   > 7 </button>
-        <button id='eight'   > 8 </button>
-        <button id='nine'    > 9 </button>
-        <button id='decimal' > . </button>
+        <button id='zero'     onClick={ handleInput }> 0 </button>
+        <button id='one'      onClick={ handleInput }> 1 </button>
+        <button id='two'      onClick={ handleInput }> 2 </button>
+        <button id='three'    onClick={ handleInput }> 3 </button>
+        <button id='four'     onClick={ handleInput }> 4 </button>
+        <button id='five'     onClick={ handleInput }> 5 </button>
+        <button id='six'      onClick={ handleInput }> 6 </button>
+        <button id='seven'    onClick={ handleInput }> 7 </button>
+        <button id='eight'    onClick={ handleInput }> 8 </button>
+        <button id='nine'     onClick={ handleInput }> 9 </button>
+        <button id='decimal'  onClick={ handleInput }> . </button>
       </div>
       <div className='operations'>
-        <button id='clear'   > AC </button>
-        <button id='add'     > + </button>
-        <button id='subtract'> - </button>
-        <button id='multiply'> * </button>
-        <button id='divide'  > / </button>
-        <button id='equals'  > = </button>
+        <button id='add'      onClick={ handleInput }> + </button>
+        <button id='subtract' onClick={ handleInput }> - </button>
+        <button id='multiply' onClick={ handleInput }> * </button>
+        <button id='divide'   onClick={ handleInput }> / </button>
+
+        <button id='clear'    onClick={ handleClear }> AC</button>
+        <button id='equals'   onClick={ handleCalc  }> = </button>
       </div>
     </div>
   )
